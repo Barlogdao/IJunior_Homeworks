8cefb3a5be89c0729ff03ee33f3dc65dd3eb6af9
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 01:08:57 +0300

    Add HomeWork1

---
+++ Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string personName;
+            byte personAge;
+            ulong secondsFromBorn;
+            float weightInKilos;
+            double heightInMeters;
+            bool isMarried;
+            decimal dollarsInAccount;
+
+            int temperatureInCelsius;
+            ushort amountOfTrees;
+            char keyCode;
+        }
+    }
+}
