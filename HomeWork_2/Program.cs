270ffcf74342b8ed26cc5b7113edeec4f45f0c7e
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 01:39:47 +0300

    Add HomeWork 2 and 4

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork_2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Кто вы по знаку зодиака?");
+            string zodiac = Console.ReadLine();
+
+            Console.WriteLine("Где вы работаете?");
+            string jobPlace = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}, вам {age} год, вы {zodiac} и работаете {jobPlace}");
+        }
+    }
+}
---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeWork_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Федотов";
+            string surname = "Александр";
+
+            Console.WriteLine(name + " " + surname);
+            (name, surname) = (surname, name);
+            Console.WriteLine(name + " " + surname);
+        }
+    }
+}
