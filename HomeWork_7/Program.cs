8b48bf868681d0afe8c74f6bce766badebcfffd8
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 02:51:03 +0300

    Add HomeWork 7

---
+++ HomeWork_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение.");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("Сколько раз его написать?");
+            int messageQuantity = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < messageQuantity; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
