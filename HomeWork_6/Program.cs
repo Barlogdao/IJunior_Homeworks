7955ec12ad5b7c3bb996c5e6d555b6b2f1ba709f
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 02:42:58 +0300

    Add HomeWork 6

---
+++ HomeWork_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int receptionDuration = 10;
+            const int minutesInHour = 60;
+
+            Console.Write("Введите количество старушек: ");
+            int  granniesAmount = int.Parse(Console.ReadLine());
+
+            int totalTime = granniesAmount * receptionDuration;
+            int hoursToReception = totalTime / minutesInHour;
+            int minutesToReception = totalTime % minutesInHour;
+            Console.WriteLine($"Вы должны отстоять в очереди {hoursToReception} часа и {minutesToReception} минут.");
+        }
+    }
+}
