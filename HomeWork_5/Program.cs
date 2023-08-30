ebba973bda1e9727a36da1cfc648b7ed780bf174
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 02:06:11 +0300

    Add HomeWork 5

---
+++ HomeWork_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeWork_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько у вас золота?");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            int crystalPrice = 5;
+            Console.WriteLine($"Предлагаем вам кристаллы по {crystalPrice} золота за штуку! Сколько возьмете?");
+            int purchasedCrystals = Convert.ToInt32(Console.ReadLine());
+            int currentGold = gold - purchasedCrystals * crystalPrice;
+            Console.WriteLine($"В вашем распоряжении {purchasedCrystals} кристаллов.\nУ вас осталось {currentGold} золота");
+        }
+    }
+}
