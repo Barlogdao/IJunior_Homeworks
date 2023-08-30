c8016f46c451a6ee6eea0cafdefd56d35dea7445
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 01:50:17 +0300

    Add Homework 3

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeWork_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInRow = 3;
+            int totalImages = 52;
+            int filledRowsAmount = totalImages / imagesInRow;
+            int leftImages = totalImages % imagesInRow;
+
+            Console.WriteLine($"Заполнено рядов: {filledRowsAmount}");
+            Console.WriteLine($"Осталось картинок: {leftImages}");
+        }
+    }
+}
