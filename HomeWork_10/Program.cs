ade2267b613bb4557e98fd73e3edbbe2776d5d02
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 03:26:36 +0300

    Add HomeWork_10

---
+++ HomeWork_10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 101);
+            int sum = 0;
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 3  == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(number);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
