2fa062f6d538853aec3a70c2fca0b5017a212f06
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 03:16:16 +0300

    Add HomeWork 9

---
+++ HomeWork_9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeWork_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int startNumber = 5;
+            const int maxNumber = 100;
+            const int increment = 7;
+            int currentNumber = startNumber;
+
+            do
+            {
+                Console.Write(currentNumber + " ");
+                currentNumber += increment;
+            }
+            while (currentNumber < maxNumber);
+        }
+    }
+}
