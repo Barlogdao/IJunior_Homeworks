3763a32dfd6b41e5ddc434df5a0824d8de3cfe52
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-31 03:02:10 +0300

    Add HomeWork_8

---
+++ HomeWork_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string exitCommand = "exit";
+            Console.WriteLine($"Для окончания выполнения программы введите \"{exitCommand}\"");
+            string input = Console.ReadLine();
+
+            while (input != exitCommand)
+            {
+                Console.WriteLine("Программы выполняется...");
+                Console.WriteLine($"Для окончания выполнения программы введите \"{exitCommand}\"");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
