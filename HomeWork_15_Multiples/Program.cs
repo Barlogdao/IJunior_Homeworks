101f59f544fc693266bf97ca8e15b842480bfc1b
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-09-05 15:41:49 +0300

    Add HW_15 Multiples

---
+++ HomeWork_15_Multiples/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeWork_15_Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minValue = 1;
+            int maxValue = 27;
+            int stepNumber = random.Next(minValue,maxValue +1);
+
+            int maxNaturalNumber = 999;
+            int naturalNumbersCount = 0;
+
+            for (int i = stepNumber; i <= maxNaturalNumber ; i+= stepNumber)
+            {
+                naturalNumbersCount++;
+            }
+
+            Console.WriteLine(stepNumber);
+            Console.WriteLine(naturalNumbersCount);
+        }
+    }
+}
