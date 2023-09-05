4cca087ffdc90ca10828bedded586fef53c6da0b
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-09-05 16:24:37 +0300

    Add HomeWork_16_PowerOfTwo

---
+++ HomeWork_16_PowerOfTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWork_16_PowerOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next();
+
+            int baseNumber = 2;
+            int exponent = 1;
+            int calculatedNumber = baseNumber;
+            
+            while (calculatedNumber <= number)
+            {
+                calculatedNumber *= baseNumber;
+                exponent++;
+            };
+            
+            Console.WriteLine($"число: {number}");
+            Console.WriteLine($"ближайшая степень {baseNumber} после числа: {exponent}");
+            Console.WriteLine($"результат возведения {baseNumber} в степень {exponent}: {calculatedNumber}");
+        }
+    }
+}
