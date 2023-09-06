be959c303d55feff527b7d17dc26ce8b5fd2c165
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-09-07 00:41:21 +0300

    Add HW_18_BossBattle

---
+++ HomeWork_18_BossBattle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork_18_BossBattle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
