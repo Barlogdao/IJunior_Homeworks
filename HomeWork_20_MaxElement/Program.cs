0a6cd3f5384e5f6afe329e5f8b8aed5bdd8c53d5
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-09-07 18:27:26 +0300

    Add HomeWork_20_MaxElement

---
+++ HomeWork_20_MaxElement/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace HomeWork_20_MaxElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxRandom = 100;
+            int[,] matrix = new int[10, 10];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(maxRandom);
+                }
+            }
+
+            int maxValue = 0;
+
+            foreach (int value in matrix)
+            {
+                if (maxValue < value)
+                    maxValue = value;
+            }
+
+            Console.WriteLine($"Наибольшие элемент: {maxValue}");
+            Console.WriteLine("\n" + "Начальная матрица матрица:");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("\n" + "Итоговая матрица:");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (maxValue == matrix[i, j])
+                        Console.Write(0 + "\t");
+                    else
+                        Console.Write(matrix[i, j] + "\t");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
