a9352d42bf5690c033d8cf8e3ec65695d418b21c
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-10-11 18:58:40 +0300

    Up to 41

--- HomeWork_039_PlayersDataBase/PlayersDataBase.cs
+++ HomeWork_039_PlayersDataBase/PlayersDataBase.cs
@@ -1,22 +1,68 @@
 ﻿namespace HomeWork_039_PlayersDataBase
 {
-    internal class PlayersDataBase
+    internal class Program
     {
         static void Main(string[] args)
         {
-            const string CommandAddPlayer = "0";
-            const string CommandRemovePlayer = "1";
-            const string CommandBanPlayer = "2";
-            const string CommandUnbanPlayer = "3";
-            const string CommandExit = "4";
+            const string CommandAddPlayer = "1";
+            const string CommandRemovePlayer = "2";
+            const string CommandBanPlayer = "3";
+            const string CommandUnbanPlayer = "4";
+            const string CommandShowAllPlayers = "5";
+            const string CommandExit = "0";
 
-            bool isExitRequested;
+            DataBase dataBase = new DataBase(true);
 
-            Console.WriteLine("Выберите ");
+            bool isExitRequested = false;
+            string inputCommand;
+            string commandMessage = $"\nВыберите команду для управления базой данных:" +
+                 $"\n [{CommandAddPlayer}] - добавить игрока в базу" +
+                 $"\n [{CommandRemovePlayer}] - удалить игрока" +
+                 $"\n [{CommandBanPlayer}] - забанить игрока" +
+                 $"\n [{CommandUnbanPlayer}] - разбанить игрока" +
+                 $"\n [{CommandShowAllPlayers}] - показать всех игроков" +
+                 $"\n [{CommandExit}] - выход" +
+                 $"\n";
 
+            while (isExitRequested == false)
+            {
+                Console.WriteLine(commandMessage);
+
+                Console.Write("Введите команду: ");
+                inputCommand = Console.ReadLine();
+
+                switch (inputCommand)
+                {
+                    case CommandAddPlayer:
+                        dataBase.AddPlayer();
+                        break;
+
+                    case CommandRemovePlayer:
+                        dataBase.RemovePlayer();
+                        break;
+
+                    case CommandBanPlayer:
+                        dataBase.BanPlayer();
+                        break;
+
+                    case CommandUnbanPlayer:
+                        dataBase.UnbanPlayer();
+                        break;
+
+                    case CommandExit:
+                        isExitRequested = true;
+                        break;
+
+                    case CommandShowAllPlayers:
+                        dataBase.ShowAllPlayers();
+                        break;
+
+                    default:
+                        Console.WriteLine("Введена неизвестная команда!");
+                        break;
+                }
+            }
         }
-
-       
     }
 
     public class DataBase
@@ -24,14 +70,27 @@
         private Dictionary<ulong, Player> _players = new Dictionary<ulong, Player>();
         private HashSet<string> _occupiedNicknames = new HashSet<string>();
         private ulong _nextAvaliableID = 0;
+
+        public DataBase(bool isFakePlayersNeeded = false)
+        {
+            if (isFakePlayersNeeded == true)
+            {
+                AppendPlayer(GetNextID(), "Destroyer", 5);
+                AppendPlayer(GetNextID(), "Killer", 23);
+                AppendPlayer(GetNextID(), "Rookie", 1);
+                AppendPlayer(GetNextID(), "Provokator", 13);
+            }
+        }
+
         public void AddPlayer()
         {
             string playerName = GetPlayerNickname();
             int playerLevel = GetPlayerLevel();
             ulong playerID = GetNextID();
 
-            _players.Add(playerID, new Player(playerID, playerName, playerLevel));
-            _occupiedNicknames.Add(playerName);
+            AppendPlayer(playerID, playerName, playerLevel);
+
+            Console.WriteLine($"Игрок добавлен. ID игрока: {playerID}");
         }
 
         public void RemovePlayer()
@@ -40,6 +99,8 @@
             {
                 _occupiedNicknames.Remove(_players[playerID].Name);
                 _players.Remove(playerID);
+
+                Console.WriteLine($"Игрок c ID {playerID} удален.");
             }
             else
             {
@@ -51,7 +112,16 @@
         {
             if (TryFindPlayerByID(out ulong playerID))
             {
+                if (_players[playerID].IsBanned)
+                {
+                    Console.WriteLine($"Игрок c ID {playerID} уже забанен.");
+
+                    return;
+                }
+
                 _players[playerID].Ban();
+
+                Console.WriteLine($"Игрок c ID {playerID} забанен.");
             }
             else
             {
@@ -63,11 +133,28 @@
         {
             if (TryFindPlayerByID(out ulong playerID))
             {
+                if (_players[playerID].IsBanned)
+                {
+                    Console.WriteLine($"Игрок c ID {playerID} не забанен.");
+
+                    return;
+                }
+
                 _players[playerID].Unban();
+
+                Console.WriteLine($"Игрок c ID {playerID} разбанен.");
             }
             else
             {
                 Console.WriteLine("Игрок с указанным ID не найден");
+            }
+        }
+
+        public void ShowAllPlayers()
+        {
+            foreach (Player player in _players.Values)
+            {
+                Console.WriteLine($"Игрок {player.Name}. ID: {player.ID}. Уровень: {player.Level}");
             }
         }
 
@@ -113,7 +200,7 @@
                 Console.Write("Введите уровень игрока: ");
                 inputLevel = Console.ReadLine();
             }
-            while (IsValidLevel(inputLevel, out level));
+            while (IsValidLevel(inputLevel, out level) == false);
 
             return level;
         }
@@ -141,6 +228,12 @@
             return _nextAvaliableID++;
         }
 
+        private void AppendPlayer(ulong playerID, string playerName, int playerLevel)
+        {
+            _players.Add(playerID, new Player(playerID, playerName, playerLevel));
+            _occupiedNicknames.Add(playerName);
+        }
+
         private bool TryFindPlayerByID(out ulong id)
         {
             Console.Write("Введите ID игрока: ");
@@ -159,31 +252,27 @@
 
     public class Player
     {
-        private readonly ulong _id;
-        private readonly string _nickName;
-        private int _level;
-        private bool _isBanned = false;
-
         public Player(ulong id, string nickName, int level)
         {
-            _id = id;
-            _nickName = nickName;
-            _level = level;
+            ID = id;
+            Name = nickName;
+            Level = level;
+            IsBanned = false;
         }
 
-        public ulong ID => _id;
-        public string Name => _nickName;
-        public int Level => _level;
-        public bool IsBanned => _isBanned;
+        public ulong ID { get; init; }
+        public string Name { get; init; }
+        public int Level { get; private set; }
+        public bool IsBanned { get; private set; }
 
         public void Ban()
         {
-            _isBanned = true;
+            IsBanned = true;
         }
 
         public void Unban()
         {
-            _isBanned = false;
+            IsBanned = false;
         }
     }
 }
---
+++ HomeWork_050_CardDeck/Program.cs
@@ -0,0 +1,172 @@
+﻿namespace HomeWork_040_CardDeck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CommandTakeCard = "1";
+            const string CommandStop = "2";
+
+            Deck deck = new Deck();
+            Player player = new Player(deck);
+            bool _isStopRequested = false;
+            string instructionMessage = $"Введите [{CommandTakeCard}] чтобы взять карту" + $"\nВведите [{CommandStop}] чтобы остановиться\n";
+
+            Console.WriteLine(instructionMessage);
+
+            while (_isStopRequested == false)
+            {
+                Console.Write("Ваш выбор: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case CommandTakeCard:
+                        player.TakeCard();
+                        break;
+
+                    case CommandStop:
+                        _isStopRequested = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Введена неверная команда!");
+                        Console.WriteLine(instructionMessage);
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nУ вас на руках:");
+            player.ShowCards();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Deck
+    {
+        private readonly Dictionary<CardRank, string> _cardRanks = new()
+        {
+            { CardRank.six,     "Шестерка"},
+            { CardRank.seven,   "Семерка"},
+            { CardRank.eight,   "Восьмерка"},
+            { CardRank.nine,    "Девятка"},
+            { CardRank.ten,     "Десятка"},
+            { CardRank.jack,    "Валет"},
+            { CardRank.queen,   "Дама"},
+            { CardRank.king,    "Король"},
+            { CardRank.ace,     "Туз"}
+        };
+
+        private readonly Dictionary<CardSuit, string> _cardSuits = new()
+        {
+            {CardSuit.hearts,   "Черви"},
+            {CardSuit.clubs,    "Треф"},
+            {CardSuit.diamonds, "Буби"},
+            {CardSuit.spades,   "Пики"}
+        };
+
+        private Stack<Card> _cards = new();
+
+        public Deck()
+        {
+            List<Card> cardList = new List<Card>(_cardRanks.Count * _cardSuits.Count);
+
+            foreach (var rank in _cardRanks.Keys)
+                foreach (var suit in _cardSuits.Keys)
+                    cardList.Add(new Card(_cardRanks[rank], _cardSuits[suit]));
+
+            Shuffle(cardList);
+
+            foreach (var card in cardList)
+            {
+                _cards.Push(card);
+            }
+        }
+
+        public Card? GetCard()
+        {
+            if (_cards.Count > 0)
+            {
+                return _cards.Pop();
+            }
+
+            return null;
+        }
+
+        private void Shuffle(List<Card> cards)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < cards.Count; i++)
+            {
+                int randomIndex = random.Next(cards.Count);
+                (cards[i], cards[randomIndex]) = (cards[randomIndex], cards[i]);
+            }
+        }
+    }
+
+    public record Card(string Rank, string Suit)
+    {
+        public override string ToString()
+        {
+            return Rank + " " + Suit;
+        }
+    }
+
+    public class Player
+    {
+        private List<Card> _hand = new();
+        private Deck _deck;
+
+        public Player(Deck deck)
+        {
+            _deck = deck;
+        }
+
+        public void TakeCard()
+        {
+            Card? nextCard = _deck.GetCard();
+
+            if (nextCard != null)
+            {
+                _hand.Add(nextCard);
+
+                Console.WriteLine($"Вы взяли {nextCard}");
+            }
+            else
+            {
+                Console.WriteLine("Карты закончились!");
+            }
+        }
+
+        public void ShowCards()
+        {
+            foreach (var card in _hand)
+            {
+                Console.WriteLine(card);
+            }
+        }
+    }
+
+    public enum CardRank
+    {
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        jack,
+        queen,
+        king,
+        ace
+    }
+
+    public enum CardSuit
+    {
+        hearts,
+        diamonds,
+        clubs,
+        spades
+    }
+}
