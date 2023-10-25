eee6a7cc64d23c319f5c2e4cdde27aa7913cb35d
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-10-26 01:46:15 +0300

    Add HW_42

--- HomeWork_041_BookStorage/Program.cs
+++ HomeWork_041_BookStorage/Program.cs
@@ -1,79 +1,277 @@
-﻿using System.Xml.Linq;
-
-namespace HomeWork_041_BookStorage
+﻿namespace HomeWork_041_BookStorage
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string CommandAddBook = "1";
+            const string CommandRemoveBook = "2";
+            const string CommandShowAllBooks = "3";
+            const string CommandFindByName = "4";
+            const string CommandFindByAuthor = "5";
+            const string CommandFindByReleaseYear = "6";
+            const string CommandExit = "0";
+
+            BookStorage bookStorage = new BookStorage();
+
+            string commandMessage = $"\nВыберите команду для работы с хранилищем книг:" +
+                $"\n [{CommandAddBook}] - добавить книгу в хранилище" +
+                $"\n [{CommandRemoveBook}] - убрать книгу из хранилища" +
+                $"\n [{CommandShowAllBooks}] - показать все книги" +
+                $"\n [{CommandFindByName}] - найти книги по названию" +
+                $"\n [{CommandFindByAuthor}] - найти книги по автору" +
+                $"\n [{CommandFindByReleaseYear}] - найти книги по году выпуска" +
+                $"\n [{CommandExit}] - выход";
+            bool isWorking = true;
+
+            Console.WriteLine(commandMessage);
+
+            while (isWorking == true)
+            {
+                Console.Write("\nВведите команду: ");
+                string input = Console.ReadLine();
+
+                Console.WriteLine();
+
+                switch (input)
+                {
+                    case CommandAddBook:
+                        bookStorage.AddBook();
+                        break;
+
+                    case CommandRemoveBook:
+                        bookStorage.RemoveBook();
+                        break;
+
+                    case CommandShowAllBooks:
+                        bookStorage.ShowAllBooks();
+                        break;
+
+                    case CommandFindByName:
+                        bookStorage.FindBooksByName();
+                        break;
+
+                    case CommandFindByAuthor:
+                        bookStorage.FindBooksByAuthor();
+                        break;
+
+                    case CommandFindByReleaseYear:
+                        bookStorage.FindBooksByReleaseYear();
+                        break;
+
+                    case CommandExit:
+                        isWorking = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Введена неизвестная команда!");
+                        Console.WriteLine(commandMessage);
+                        break;
+                }
+            }
         }
     }
 
     public class BookStorage
     {
-        private List<Book> _books = new();
+        private readonly List<Book> _books;
 
-        public bool HasBook(Book book)
+        public BookStorage()
         {
-            return _books.Contains(book);
+            _books = new List<Book>()
+            {
+                new Book("Автостопом по галактике", "Дуглас Адамс", 1979),
+                new Book("1984", "Джордж Оруэлл", 1949),
+                new Book("Скотный двор", "Джордж Оруэлл", 1945),
+                new Book("Мы", "Евгений Замятин", 1920),
+                new Book("Сердца трех", "Джек Лондон", 1920),
+                new Book("Алхимик", "Пауло Коэльо", 1988),
+                new Book("Алхимик", "Говард Лавкрфакт", 1908),
+            };
         }
 
-        public void AddBook(Book book)
+        public void AddBook()
         {
-            _books.Add(book);
+            Console.WriteLine("Выбрано добавление книги в хранилище");
+
+            string bookName = GetInput("Укажите название книги");
+            string authorName = GetInput("Укажите имя автора"); ;
+            int releaseYear = GetReleaseYear("Укажите год выпуска книги");
+
+            Book book = new Book(bookName, authorName, releaseYear);
+
+            if (HasBook(book))
+            {
+                Console.WriteLine("Такая книга уже есть в хранилище");
+            }
+            else
+            {
+                AppendBook(book);
+            }
         }
 
-        public void RemoveBook(Book book)
+        public void RemoveBook()
         {
-            _books.Remove(book);
+            Console.WriteLine("Выбрано удаление книги из хранилища");
+
+            int bookNumber = GetBookNumber();
+            int bookIndex = bookNumber - 1;
+
+            if (bookIndex < 0 || bookIndex >= _books.Count)
+            {
+                Console.WriteLine("В хранилище отсутствует книга с указанным номером");
+            }
+            else
+            {
+                Book bookForRemove = _books[bookIndex];
+
+                Console.WriteLine($"Книга \"{bookForRemove.Name}\" удалена из хранилища.");
+
+                _books.Remove(bookForRemove);
+            }
         }
 
         public void ShowAllBooks()
         {
             foreach (Book book in _books)
             {
-                Console.WriteLine(book);
+                DisplayBook(book);
             }
         }
 
-        public void ShowBooksByName(string name)
+        public void FindBooksByName()
         {
+            Console.WriteLine("Выбран поиск книг по названию");
+
+            string bookName = GetInput("Укажите название книг");
+
             foreach (Book book in _books)
             {
-                if (book.Name == name)
-                Console.WriteLine(book);
+                if (book.Name.ToLower() == bookName.ToLower())
+                    DisplayBook(book);
             }
         }
 
-        public void ShowBooksByAuthor(string author)
+        public void FindBooksByAuthor()
         {
+            Console.WriteLine("Выбран поиск книг по автору");
+
+            string authorName = GetInput("Укажите автора");
+
             foreach (Book book in _books)
             {
-                if (book.Author == author)
-                    Console.WriteLine(book);
+                if (book.Author.ToLower() == authorName.ToLower())
+                    DisplayBook(book);
             }
         }
 
-        public void ShowBooksByYear(int releaseYear)
+        public void FindBooksByReleaseYear()
         {
+            Console.WriteLine("Выбран поиск книг по году выпуска");
+
+            int releaseYear = GetReleaseYear("Укажите год выпуска книг");
+
             foreach (Book book in _books)
             {
                 if (book.ReleaseYear == releaseYear)
-                    Console.WriteLine(book);
+                    DisplayBook(book);
             }
+        }
+
+        private string GetInput(string message)
+        {
+            Console.Write(message + ": ");
+            string inputString = Console.ReadLine();
+
+            return inputString;
+        }
+
+        private int GetReleaseYear(string message)
+        {
+            int releaseYear;
+            string inputYear;
+
+            do
+            {
+                inputYear = GetInput(message);
+            }
+            while (IsValidReleaseYear(inputYear, out releaseYear) == false);
+
+            return releaseYear;
+        }
+
+        private bool IsValidReleaseYear(string inputYear, out int releaseYear)
+        {
+            int minYear = 998;
+            int maxYear = 2023;
+
+            if (int.TryParse(inputYear, out releaseYear) == false)
+            {
+                Console.WriteLine("Некорректный ввод! Значение должно быть числом.");
+
+                return false;
+            }
+
+            if (releaseYear < minYear || releaseYear > maxYear)
+            {
+                Console.WriteLine($"Некорректный год выпуска книги! Год выпуска может быть в периоде между {minYear} и {maxYear} годом.");
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool HasBook(Book book)
+        {
+            return _books.Contains(book);
+        }
+
+        private void AppendBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        private int GetBookNumber()
+        {
+            int bookNumber;
+            string userInput;
+
+            userInput = GetInput("Укажите номер книги в хранилище");
+
+            while (int.TryParse(userInput, out bookNumber) == false)
+            {
+                Console.WriteLine("Номер книги должен быть числом!");
+
+                userInput = GetInput("Укажите номер книги в хранилище");
+            }
+
+            return bookNumber;
+        }
+
+        private void DisplayBook(Book book)
+        {
+            int countOffset = 1;
+            Console.WriteLine((_books.IndexOf(book) + countOffset) + ". " + book);
         }
     }
 
-    public record Book(string Name, string Author, int ReleaseYear)
+    public readonly struct Book
     {
+        public Book(string name, string author, int releaseYear)
+        {
+            Name = name;
+            Author = author;
+            ReleaseYear = releaseYear;
+        }
+
+        public string Name { get; init; }
+        public string Author { get; init; }
+        public int ReleaseYear { get; init; }
+
         public override string ToString()
         {
-            return $"Книга {Name}. Автор {Author}. Год выпуска {ReleaseYear}.";
+            return $"{Name} ({ReleaseYear}). {Author}";
         }
     }
 }
-
-//Создать хранилище книг. 
-//Каждая книга имеет название, автора и год выпуска (можно добавить еще параметры).
-//В хранилище можно добавить книгу, убрать книгу, показать все книги и показать книги по указанному параметру (по названию, по автору, по году выпуска).
---
+++ HomeWork_042_Shop/Program.cs
@@ -0,0 +1,218 @@
+﻿namespace HomeWork_042_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(100);
+            Seller seller = new Seller();
+            Shop shop = new Shop(player, seller);
+
+            shop.Run();
+        }
+    }
+
+    public class Shop
+    {
+        private const string CommandShowItems = "1";
+        private const string CommandBuyItem = "2";
+        private const string CommandShowInventory = "3";
+        private const string CommandExit = "0";
+
+        private Player _player;
+        private Seller _seller;
+
+        private bool _isWorking = true;
+        private string _commandMessage = $"\n [{CommandShowItems}] - посмотреть товары торговца" +
+                $"\n [{CommandBuyItem}] - купить товар у торговца" +
+                $"\n [{CommandShowInventory}] - посмотреть свой инвентарь" +
+                $"\n [{CommandExit}] - выход";
+
+        public Shop(Player player, Seller seller)
+        {
+            _player = player;
+            _seller = seller;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Добро пожаловать в лавку торговца!");
+            Console.WriteLine(_commandMessage);
+
+            while (_isWorking)
+            {
+                Console.Write("\nВведите команду: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case CommandShowItems:
+                        _seller.ShowItems();
+                        break;
+
+                    case CommandShowInventory:
+                        _player.ShowInventory();
+                        break;
+
+                    case CommandBuyItem:
+                        Console.Write("Укажите номер товара: ");
+                        input = Console.ReadLine();
+
+                        if (int.TryParse(input, out int itemNumber))
+                        {
+                            int itemIndex = itemNumber - 1;
+                            Buy(_player, _seller, itemIndex);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Номер товара должен быть числом!");
+                        }
+                        break;
+
+                    case CommandExit:
+                        _isWorking = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверная команда!");
+                        Console.WriteLine(_commandMessage);
+                        break;
+                }
+            }
+        }
+
+        private void Buy(Player player, Seller seller, int itemIndex)
+        {
+            Item item;
+            if (seller.TryGetItem(itemIndex, out item))
+            {
+                if (player.CanBuyItem(item))
+                {
+                    player.BuyItem(item);
+                    seller.SellItem(item);
+                }
+                else
+                {
+                    Console.WriteLine($"У вас недостаточно средств для покупки предмета ({item.Name}).");
+                }
+            }
+        }
+    }
+
+    public class Player
+    {
+        private int _money;
+        private readonly List<Item> _inventory;
+
+        public Player(int money)
+        {
+            _money = money;
+            _inventory = new();
+        }
+
+        public void ShowInventory()
+        {
+            Console.WriteLine("В вашем инвентаре:");
+            Console.WriteLine($"Золото ({_money})");
+
+            foreach (Item item in _inventory)
+                Console.WriteLine(item.Name);
+        }
+
+        public bool CanBuyItem(Item item)
+        {
+            return _money >= item.Price;
+        }
+
+        public void BuyItem(Item item)
+        {
+            _money -= item.Price;
+            AddItemToInventory(item);
+
+            Console.WriteLine($"Вы купили предмет ({item.Name})");
+        }
+
+        private void AddItemToInventory(Item item)
+        {
+            _inventory.Add(item);
+        }
+    }
+
+    public class Seller
+    {
+        private readonly List<Item> _items;
+
+        public Seller()
+        {
+            _items = new List<Item>()
+            {
+                new Item("Меч", 25),
+                new Item("Щит", 20),
+                new Item("Лук", 22),
+                new Item("Конь", 30),
+            };
+        }
+
+        public void ShowItems()
+        {
+            if (_items.Count == 0)
+            {
+                Console.WriteLine("Торговцу нечего вам предложить(");
+
+            }
+            else
+            {
+                int countOffset = 1;
+
+                Console.WriteLine("Торговец предлагает следующие товары:");
+
+                for (int i = 0; i < _items.Count; i++)
+                    Console.WriteLine($"{i + countOffset}. {_items[i]}");
+            }
+        }
+
+        public bool TryGetItem(int itemIndex, out Item item)
+        {
+            item = null;
+
+            if (itemIndex < 0 || itemIndex >= _items.Count)
+            {
+                Console.WriteLine("Предмета с указанным номером не существует!");
+                return false;
+            }
+
+            item = _items[itemIndex];
+            return true;
+        }
+
+        public Item SellItem(Item item)
+        {
+            _items.Remove(item);
+            return item;
+        }
+    }
+
+    public class Item
+    {
+        public Item(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; init; }
+        public int Price { get; init; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price} золотых.";
+        }
+    }
+}
+//Существует продавец, он имеет у себя список товаров,
+//и при нужде, может вам его показать, также продавец может продать вам товар.
+//После продажи товар переходит к вам, и вы можете также посмотреть свои вещи. 
+
+//Возможные классы – игрок, продавец, товар. 
+
+//Вы можете сделать так, как вы видите это.
