b0436ab8b436c483704569af4ac40051a59222c0
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-11-20 17:49:59 +0300

    Start HW_045_Supermarket

--- HomeWork_043_TrainConfigurator/Program.cs
+++ HomeWork_043_TrainConfigurator/Program.cs
@@ -4,49 +4,328 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string CommandCreateDirection = "1";
+            const string CommandSellTickets = "2";
+            const string CommandCreateTrain = "3";
+            const string CommandSendTrain = "4";
+            const string CommandExit = "0";
+
+            RailWayStation railwayStation = new RailWayStation();
+            string commandMessage = $"\nВведите команду" +
+                $"\n[{CommandCreateDirection}] - создать направление" +
+                $"\n[{CommandSellTickets}] - продать билеты" +
+                $"\n[{CommandCreateTrain}] - сформировать поезд" +
+                $"\n[{CommandSendTrain}] - отправить поезд" +
+                $"\n[{CommandExit}] - выйти";
+            bool isRun = true;
+
+            while (isRun)
+            {
+                railwayStation.DisplayInfo();
+                Console.WriteLine(commandMessage);
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case CommandCreateDirection:
+                        railwayStation.CreateDirection();
+                        break;
+
+                    case CommandSellTickets:
+                        railwayStation.SellTickets();
+                        break;
+
+                    case CommandCreateTrain:
+                        railwayStation.CreateTrain();
+                        break;
+
+                    case CommandSendTrain:
+                        railwayStation.SendTrain();
+                        break;
+
+                    case CommandExit:
+                        isRun = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Введена неизвестная команда!");
+                        break;
+                }
+
+                Console.WriteLine("\nДля продолжения нажмите любую клавишу");
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+
+    public class RailWayStation
+    {
+        private int _money = 0;
+        private int _passenegers;
+        private State _state = State.Avaliable;
+        private Direction _createdDirection = null;
+        private Train _createdTrain = null;
+        private Route _createdRoute = null;
+
+        private string _startPoint = "Москва";
+        private string[] _destinationPoints = new string[] { "Екатеринбург", "Сочи", "Казань" };
+
+        private Random _random = new Random();
+
+        public void CreateDirection()
+        {
+            if(_state != State.Avaliable)
+            {
+                Console.WriteLine("Направление уже сформировано!");
+                DisplayNextStep();
+                return;
+            }
+
+            _createdDirection = new Direction(_startPoint, GetDestinationPoint());
+            _state = State.DirectionCreated;
+
+            Console.WriteLine($"Создано направление {_createdDirection}");
+        }
+
+        public void SellTickets()
+        {
+            if (_state != State.DirectionCreated)
+            {
+                Console.WriteLine("Невозможно продать билеты!");
+
+                DisplayNextStep();
+                return;
+            }
+
+            int maxPassenegersAmount = 300;
+
+            _passenegers = _random.Next(maxPassenegersAmount + 1);
+            int earnedMoney = _passenegers * _createdDirection.TicketPrice;
+            _money += earnedMoney;
+            _state = State.TicketsSold;
+
+            Console.WriteLine($"Продано билетов: {_passenegers}");
+            Console.WriteLine($"Получено: {earnedMoney} рублей");
+            Console.WriteLine($"В кассе: {_money} рублей");
+        }
+
+        public void CreateTrain()
+        {
+            if (_state != State.TicketsSold)
+            {
+                Console.WriteLine("Невозможно сформировать поезд!");
+
+                DisplayNextStep();
+                return;
+            }
+
+            _createdTrain = new Train(_passenegers, _random.Next());
+            _state = State.TrainCreated;
+
+            Console.WriteLine($"Сформирован {_createdTrain} на {_createdTrain.TotalSeats} мест");
+        }
+
+        public void SendTrain()
+        {
+            if (_state != State.TrainCreated)
+            {
+                Console.WriteLine("Невозможно отправить поезд!");
+
+                DisplayNextStep();
+                return;
+            }
+
+            _createdRoute = new Route(_createdDirection, _createdTrain);
+            _state = State.Avaliable;
+
+            Console.WriteLine($"На рейс {_createdDirection} вышел {_createdTrain}");
+        }
+
+        public void DisplayInfo()
+        {
+            if (_createdRoute != null)
+            {
+                _createdRoute.ShowInfo();
+            }
+            else
+            {
+                Console.WriteLine("На сегодня рейсов нет");
+            }
+        }
+
+        private string GetDestinationPoint()
+        {
+            return _destinationPoints[_random.Next(0, _destinationPoints.Length)];
+        }
+
+        private void DisplayNextStep()
+        {
+            switch (_state)
+            {
+                case State.Avaliable:
+                    Console.WriteLine("Необходимо создать направление");
+                    break;
+
+                case State.DirectionCreated:
+                    Console.WriteLine("Необходимо продать билеты");
+                    break;
+
+                case State.TicketsSold:
+                    Console.WriteLine("Необходимо сформировать поезд");
+                    break;
+                case State.TrainCreated:
+                    Console.WriteLine("Необходимо отправить поезд");
+                    break;
+            };
+        }
+
+        private enum State
+        {
+            Avaliable,
+            DirectionCreated,
+            TicketsSold,
+            TrainCreated
+        }
+    }
+
+    public class Direction
+    {
+        public Direction(string startPoint, string destinationPoint)
+        {
+            TicketPrice = GetTicketCost();
+            StartPoint = startPoint;
+            DestinationPoint = destinationPoint;
+        }
+
+        public int TicketPrice { get; private set; }
+        public string StartPoint { get; private set; }
+        public string DestinationPoint { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{StartPoint} - {DestinationPoint}";
+        }
+
+        private int GetTicketCost()
+        {
+            int minTicketPrice = 200;
+            int maxTicketPrice = 2000;
+            Random random = new Random();
+
+            return random.Next(minTicketPrice, maxTicketPrice);
         }
     }
 
     public class Train
     {
+        private readonly List<Carriage> _carriages = new List<Carriage>();
+        private int _passenegersAmount;
+        private int _number;
 
+        public Train(int passenegersAmount, int number)
+        {
+            _passenegersAmount = passenegersAmount;
+            _number = number;
+            Build();
+            AccomodatePasseners();
+        }
+
+        public int TotalSeats { get; private set; }
+
+        public override string ToString()
+        {
+            return $"поезд # {_number}";
+        }
+
+        public void AccomodatePasseners()
+        {
+            int leftPassenegers = _passenegersAmount;
+
+            foreach (Carriage carriage in _carriages)
+            {
+                carriage.AccomodatePassenegers(ref leftPassenegers);
+            }
+        }
+
+        private void Build()
+        {
+            Carriage carriage;
+
+            while (_passenegersAmount > TotalSeats)
+            {
+                carriage = new Carriage();
+                _carriages.Add(carriage);
+                TotalSeats += carriage.TotalSeats;
+            }
+        }
+
+        public void ShowInfo()
+        {
+            int counter = 1;
+
+            Console.WriteLine(this);
+            Console.WriteLine($"Всего мест {TotalSeats}");
+
+            foreach (var carriage in _carriages)
+            {
+                Console.WriteLine($"Вагон #{counter++}. {carriage}");
+            }
+        }
     }
 
-    public class Passenger
+    public class Carriage
     {
+        public Carriage()
+        {
+            TotalSeats = GetTotalSeats();
+            AvaliableSeats = TotalSeats;
+        }
 
+        public int TotalSeats { get; init; }
+        public int AvaliableSeats { get; private set; }
+
+        public void AccomodatePassenegers(ref int leftPassenegers)
+        {
+            int occupiedSeats = Math.Min(TotalSeats, leftPassenegers);
+            AvaliableSeats -= occupiedSeats;
+            leftPassenegers -= occupiedSeats;
+        }
+
+        private int GetTotalSeats()
+        {
+            const int MinSeatsValue = 20;
+            const int MaxSeatsValue = 45;
+
+            Random random = new Random();
+
+            return random.Next(MinSeatsValue, MaxSeatsValue);
+        }
+
+        public override string ToString()
+        {
+            return $"Свободных мест: {AvaliableSeats} из {TotalSeats}";
+        }
     }
 
-    public class TrainConfigurator
+    public class Route
     {
-        public void CreatePath()
-        {
+        private readonly Direction _direction;
+        private readonly Train _train;
 
+        public Route(Direction direction, Train train)
+        {
+            _direction = direction;
+            _train = train;
         }
 
-        public void SellTickets()
+        public void ShowInfo()
         {
-
-        }
-
-        public void CreateTrain()
-        {
-
-        }
-
-        public void SendTrain()
-        {
-
+            Console.WriteLine($"Действующий рейс: {_direction}.");
+            Console.Write($"На рейс вышел ");
+            _train.ShowInfo();
         }
     }
 }
-
-//У вас есть программа, которая помогает пользователю составить план поезда.
-//Есть 4 основных шага в создании плана:
-//-Создать направление - создает направление для поезда(к примеру Бийск - Барнаул)
-//-Продать билеты - вы получаете рандомное кол-во пассажиров, которые купили билеты на это направление
-//-Сформировать поезд - вы создаете поезд и добавляете ему столько вагонов(вагоны могут быть разные по вместительности),
-//сколько хватит для перевозки всех пассажиров.
-//-Отправить поезд - вы отправляете поезд, после чего можете снова создать направление.
-//В верхней части программы должна выводиться полная информация о текущем рейсе или его отсутствии.
---
+++ HomeWork_044_GladiatorsFight/Program.cs
@@ -0,0 +1,360 @@
+﻿namespace HomeWork_044_GladiatorsFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FighterLobby fighterLobby = new FighterLobby();
+            fighterLobby.PrepareFighters();
+
+            Pit pit = new Pit(fighterLobby.FirstFighter, fighterLobby.SecondFighter);
+            pit.StartFight();
+        }
+    }
+
+    public class FighterLobby
+    {
+        private const string CommandWarrior = "1";
+        private const string CommandVampire = "2";
+        private const string CommandDuelist = "3";
+        private const string CommandBloodWizard = "4";
+        private const string CommandBandit = "5";
+
+        private Dictionary<string, Fighter> _fighters = new()
+        {
+            { CommandWarrior, new Warrior() },
+            { CommandVampire, new Vampire() },
+            { CommandDuelist, new Duelist() },
+            { CommandBloodWizard, new BloodWizard() },
+            { CommandBandit, new Bandit() },
+        };
+
+        public Fighter FirstFighter { get; private set; }
+        public Fighter SecondFighter { get; private set; }
+
+        public void PrepareFighters()
+        {
+            Console.WriteLine("Добро пожаловать в Яму!");
+
+            ShowFighters();
+            SelectFighters();
+        }
+
+        private void ShowFighters()
+        {
+            Console.WriteLine("Выберите гладиаторов для сражения в Яме:");
+
+            foreach (string fighterKey in _fighters.Keys)
+            {
+                Console.Write($"[{fighterKey}] - ");
+                _fighters[fighterKey].ShowInfo();
+            }
+        }
+
+        private void SelectFighters()
+        {
+            FirstFighter = GetFighter("\nВыберите первого гладиатора: ");
+            SecondFighter = GetFighter("\nВыберите второго гладиатора: ");
+        }
+
+        private Fighter GetFighter(string message)
+        {
+            Fighter fighter = null;
+
+            while (fighter == null)
+            {
+                Console.Write(message);
+                string input = Console.ReadLine();
+
+                fighter = input switch
+                {
+                    CommandWarrior => new Warrior(),
+                    CommandVampire => new Vampire(),
+                    CommandBloodWizard => new BloodWizard(),
+                    CommandDuelist => new BloodWizard(),
+                    CommandBandit => new Bandit(),
+                    _ => null
+                };
+
+                if (fighter == null)
+                {
+                    Console.WriteLine("Введена неверная команда!");
+                }
+            }
+
+            Console.WriteLine($"{fighter} выходит на арену");
+
+            return fighter;
+        }
+    }
+
+    public class Pit
+    {
+        private Fighter _firstFighter;
+        private Fighter _secondFighter;
+
+        public Pit(Fighter firstFighter, Fighter secondFighter)
+        {
+            _firstFighter = firstFighter;
+            _secondFighter = secondFighter;
+        }
+
+        public void StartFight()
+        {
+            Console.WriteLine("\nБитва Началась!");
+
+            WaitForInput();
+
+            while (_firstFighter.IsAlive && _secondFighter.IsAlive)
+            {
+                Console.WriteLine($"{_firstFighter} атакует {_secondFighter}");
+                _firstFighter.Attack(_secondFighter);
+
+                WaitForInput();
+
+                Console.WriteLine($"{_secondFighter} атакует {_firstFighter}");
+                _secondFighter.Attack(_firstFighter);
+
+                WaitForInput();
+            }
+
+            ShowFightResult();
+        }
+
+        private void WaitForInput()
+        {
+            Console.WriteLine("\nНажмите любую клавишу чтобы продолжить");
+            Console.ReadKey();
+        }
+
+        private void ShowFightResult()
+        {
+            Console.WriteLine("Битва окончена!");
+
+            if (_firstFighter.IsAlive)
+            {
+                Console.WriteLine($"Победил {_firstFighter}");
+            }
+            else if (_secondFighter.IsAlive)
+            {
+                Console.WriteLine($"Победил {_secondFighter}");
+            }
+            else
+            {
+                Console.WriteLine("Оба гладиатора погибли. Несите новых!");
+            }
+        }
+    }
+
+    public abstract class Fighter
+    {
+        protected readonly int MaxHealth;
+        protected int CurrentHealth;
+        protected readonly int Damage;
+        protected readonly string Name;
+
+        public Fighter(int health, int damage)
+        {
+            MaxHealth = health;
+            CurrentHealth = MaxHealth;
+            Damage = damage;
+            Name = NameGenerator.GetName();
+        }
+
+        public bool IsAlive => CurrentHealth > 0;
+        public abstract string Class { get; }
+
+        protected abstract string AbilityInfo { get; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Class}. Здоровье: {MaxHealth}. Урон: {Damage}. {AbilityInfo}");
+        }
+
+        public virtual void Attack(Fighter enemy)
+        {
+            enemy.TakeDamage(Damage);
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+            CurrentHealth -= Math.Max(0, damage); ;
+            Console.WriteLine($"{this} получил {damage} урона");
+        }
+
+        public override string ToString()
+        {
+            return $"{Class} {Name}";
+        }
+    }
+
+    public class Warrior : Fighter
+    {
+        private readonly int _armor;
+
+        public Warrior() : base(100, 18)
+        {
+            _armor = 3;
+        }
+
+        public override string Class => "Воин";
+
+        protected override string AbilityInfo => $"Броня: Уменьшает получаемый урон на {_armor}";
+
+        public override void TakeDamage(int damage)
+        {
+            CurrentHealth -= Math.Max(0, damage - _armor); ;
+            Console.WriteLine($"{this} получил {damage - _armor} урона");
+        }
+    }
+
+    public class Vampire : Fighter
+    {
+        private readonly int _healAmount;
+        private readonly int _healChance;
+        private readonly int _maxHealChance = 100;
+
+        public Vampire() : base(70, 15)
+        {
+            _healAmount = 10;
+            _healChance = 30;
+        }
+
+        public override string Class => "Вампир";
+        protected override string AbilityInfo => $"Лечение: Шанс {_healChance}% восстановить при атаке {_healAmount} здоровья ";
+
+        public override void Attack(Fighter enemy)
+        {
+            TryHeal();
+            enemy.TakeDamage(Damage);
+        }
+
+        private void TryHeal()
+        {
+            if (RandomProvider.Random.Next(_maxHealChance + 1) <= _healChance)
+            {
+                int restoredHealth = (CurrentHealth + _healAmount) > MaxHealth ? MaxHealth - CurrentHealth : _healAmount;
+                CurrentHealth += restoredHealth;
+
+                Console.WriteLine($"{this} восстановил {restoredHealth} здоровья.");
+            }
+        }
+    }
+
+    public class Duelist : Fighter
+    {
+        private readonly int _maxAttackCounter;
+        private int _attackCounter = 0;
+        private readonly int _damageMultiplier;
+
+        public Duelist() : base(80, 14)
+        {
+            _maxAttackCounter = 3;
+            _damageMultiplier = 2;
+        }
+
+        public override string Class => "Дуэлянт";
+        protected override string AbilityInfo => $"Концентрация: Каждая {_maxAttackCounter} атака наносит {Damage * _damageMultiplier} урона";
+
+        public override void Attack(Fighter enemy)
+        {
+            _attackCounter++;
+
+            if (_attackCounter >= _maxAttackCounter)
+            {
+                _attackCounter = 0;
+
+                Console.WriteLine($"{this} наносит критический урон!");
+
+                enemy.TakeDamage(Damage * _damageMultiplier);
+            }
+            else
+            {
+                enemy.TakeDamage(Damage);
+            }
+        }
+    }
+
+    public class BloodWizard : Fighter
+    {
+        private readonly int _maxBloodPoints;
+        private int _currentBloodPoints = 0;
+
+        public BloodWizard() : base(120, 10)
+        {
+            _maxBloodPoints = 45;
+        }
+
+        public override string Class => "Маг крови";
+        protected override string AbilityInfo => $"Щит Крови: получаемый урон гененрирует очки Крови." +
+            $" Заряженный Щит Крови ({_maxBloodPoints} очков Крови) предотвращает урон от следующей атаки.";
+
+        public override void TakeDamage(int damage)
+        {
+            if (_currentBloodPoints >= _maxBloodPoints)
+            {
+                Console.WriteLine($"{this} использовал Щит Крови. Урон предотвращен");
+
+                _currentBloodPoints = 0;
+            }
+            else
+            {
+                CurrentHealth -= Math.Max(0, damage);
+
+                Console.WriteLine($"{this} получил {damage} урона");
+
+                _currentBloodPoints += damage;
+
+                if (_currentBloodPoints >= _maxBloodPoints)
+                {
+                    Console.WriteLine($"{this} зарядил Щит Крови");
+                }
+            }
+        }
+    }
+
+    public class Bandit : Fighter
+    {
+        private int _bonusDamage = 0;
+
+        public Bandit() : base(90, 13) { }
+
+        public override string Class => "Бандит";
+        protected override string AbilityInfo => $"Возмездие: Получение урона увеличивает наносимый урон на 1";
+
+        public override void Attack(Fighter enemy)
+        {
+            enemy.TakeDamage(Damage + _bonusDamage);
+        }
+
+        public override void TakeDamage(int damage)
+        {
+            base.TakeDamage(damage);
+            _bonusDamage++;
+        }
+    }
+
+    public static class NameGenerator
+    {
+        private static string[] _names = new string[]
+        {
+            "Василий",
+            "Максимус",
+            "Страус",
+            "Глок",
+            "Эскимо"
+        };
+
+        public static string GetName()
+        {
+            return _names[RandomProvider.Random.Next(0, _names.Length)];
+        }
+    }
+
+    public static class RandomProvider
+    {
+        private static Random _random = new Random();
+
+        public static Random Random => _random;
+    }
+}
